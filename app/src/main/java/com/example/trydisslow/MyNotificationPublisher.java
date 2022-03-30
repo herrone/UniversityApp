5dd5d5b10cda6025acdf5e83f341360c5baf8c1b
herrone <egretfulherron@gmail.com>
2022-03-30 22:52:34 +0100

    to-do; - [ ] add pop ups for info for assignments and classes in the screens - [ ] get a sensor in? - [ ] grade calculator screen? - [ ] add tests - [ ] notifications

---
+++ app/src/main/java/com/example/trydisslow/MyNotificationPublisher.java
@@ -0,0 +1,31 @@
+package com.example.trydisslow;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Build;
+import android.os.Bundle;
+public class MyNotificationPublisher extends BroadcastReceiver {
+
+    public static String NOTIFICATION_ID = "notification_id";
+    public static String NOTIFICATION = "notification";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int notificationId = intent.getIntExtra(NOTIFICATION_ID, 0);
+        notificationManager.notify(notificationId, notification);
+    }
+}
---
+++ app/src/main/java/com/example/trydisslow/PrintableClass.java
@@ -0,0 +1,39 @@
+package com.example.trydisslow;
+
+import android.text.format.Time;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PrintableClass {
+
+  Class contained;
+  Date dueDate;
+
+    public PrintableClass(){
+
+    }
+    public PrintableClass(Class a, Date d){
+
+        contained = a;
+        dueDate = d;
+
+
+    }
+//    public ArrayList<Class> makeArraylistClasses(Class a){
+//        ArrayList<Class> classes = new ArrayList<>();
+//
+//
+//
+//    return classes;
+//    }
+
+}
+
+
+
+
+
+
+
